a299acf57182977b3fa35411d05a1a022a0dd271
dptlfccm01 <dptlfccm01@gmail.com>
2014-10-01 01:20:32 +0100

    renamed package

--- SpringProjectTest/src/main/java/com/springprojecttest/app/controller/SampleController.java
+++ SpringProjectTest/src/main/java/com/springprojecttest/app/controller/SampleController.java
@@ -1,4 +1,4 @@
-package com.example.app.MyNewProject;
+package com.springprojecttest.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
